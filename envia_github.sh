aed2ebedda7e9e1e8f94ed7ea6029c1cc2f5459b
Edgard <edgardiniz.bittencourt@gmail.com>
2017-05-22 19:52:21 -0300

    script para envio ao github

--- envia_github.sh
+++ envia_github.sh
@@ -1,4 +1,6 @@
 #!bin/bash
+git config --global user.name  "Edgard"
+git config --global user.email edgardiniz.bittencourt@gmail.com
 git add *
 git commit -m "script para envio ao github"
 git push
